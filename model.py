d86d45e0d413b54f746c0c96e8f9a56f6c9e480c
PC <pmphuong1704@gmail.com>
2023-10-31 04:29:55 +0700

    new cmt

--- model.py
+++ model.py
@@ -6,9 +6,10 @@ class MyModel(nn.Module):
     def __init__(self, num_classes = 53):
         super().__init__()
         self.base_model = timm.create_model('efficientnet_b0', pretrained = True)
-        self.feature_extractor = nn.Sequential(*list(self.base_model.children()))[:-1] # output = 1280
-        self.classifier = nn.Linear(in_features = 1280, out_features = num_classes) # output = num_classes
+        # remove the classifier layer of efficientnet_b0
+        self.feature_extractor = nn.Sequential(*list(self.base_model.children()))[:-1] 
+        self.classifier = nn.Linear(in_features = 1280, out_features = num_classes) # new head of model
 
     def forward(self, x):
         x = self.feature_extractor(x)
-        return self.classifier(x) # (32, 53) ~ (batch_size, num_classes) 
+        return self.classifier(x) # (batch_size, num_classes) 

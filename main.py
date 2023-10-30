dbe2230d52cba9ea9d9f4493d205d6733822e77e
PC <pmphuong1704@gmail.com>
2023-10-30 18:15:34 +0700

    convert to scripts

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# daf
-"""
-- trainer
-    - training loop
-    - optimizer
-    - scheduler
-    - model
-"""
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from tqdm import tqdm
+from model import *
+from dataset import *
 
-import torch.nn as nn
+if __name__ == '__main__':
+    transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((128,128))
+    ])
+    train_folder = "./cards-image-datasetclassification/train"
+    test_folder = "./cards-image-datasetclassification/test"
 
-class Model(nn.Moudle):
-    def __init__(self, *args, **kwargs): pass
-    def fit(self, train_dataset, batch_size, epochs): pass
+    train_dataset = PlayingCardDataset(data_dir = train_folder,
+                                    transform = transform)
+    test_dataset = PlayingCardDataset(data_dir = test_folder,
+                                    transform = transform)
 
-f
+    train_dataloader = DataLoader(dataset = train_dataset,
+                                batch_size = 32,
+                                shuffle = True)
+    test_dataloader = DataLoader(dataset = test_dataset,
+                                batch_size = 32,
+                                shuffle = False)
+    
+    model = MyModel()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+    epochs = 5
+    device = "cuda" if torch.cuda.is_available() else "gpu"
+    print(f"Using {device}")
+    print("-"*50)
+    train_losses = []
+    test_losses = []
+
+    model.train()
+    for epoch in range(epochs):
+        running_loss = 0
+        for image, label in tqdm(train_dataloader):
+            image = image.to(device)
+            label = label.to(device)
+            model = model.to(device)
+            model.zero_grad()
+            # forward pass
+            prediction = model(image)
+            loss = loss_fn(prediction, label)
+            # backward
+            running_loss += loss * image.shape[0] # loss * batch_size (it looks like AverageMeter())
+            loss.backward()
+            optimizer.step()
+        train_loss = running_loss / len(train_dataloader) # loss of current epoch
+        train_losses.append(train_loss)
+        print(f"Epochs {epoch}: Training loss: {train_loss:.2f}")
+        break
---
+++ model.py
@@ -0,0 +1,14 @@
+
+from torch import nn
+import timm
+
+class MyModel(nn.Module):
+    def __init__(self, num_classes = 53):
+        super().__init__()
+        self.base_model = timm.create_model('efficientnet_b0', pretrained = True)
+        self.feature_extractor = nn.Sequential(*list(self.base_model.children()))[:-1] # output = 1280
+        self.classifier = nn.Linear(in_features = 1280, out_features = 53) # output = num_classes
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        return self.classifier(x) # (32, 53) ~ (batch_size, num_classes) 
